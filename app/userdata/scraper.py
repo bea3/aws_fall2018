f5da1fa5f26b0b3628da127b8b85dc9fb09aa341
Beatrice <bea3@umbc.edu>
2018-12-03 22:31:44 -0500

    more changes

--- app/userdata/scraper.py
+++ app/userdata/scraper.py
@@ -2,12 +2,12 @@ import pychrome
 import os
 import json
 
-if 'TARGET_URL' not in os.environ or os.environ["TARGET_URL"] == "THISISTHETARGETURL":
-    TARGET_URL = "https://slickdeals.net"
-else:
-    TARGET_URL = os.environ["TARGET_URL"]
+target_url = os.environ.get('TARGET_URL')
 
-print(TARGET_URL)
+if target_url is None or target_url == "THISISTHETARGETURL":
+    target_url = "https://slickdeals.net"
+
+print(os.environ.get('TARGET_URL'))
 
 browser = pychrome.Browser(url="http://127.0.0.1:9222")
 tab = browser.new_tab()
@@ -21,7 +21,7 @@ tab.set_listener("Network.requestWillBeSent", request_will_be_sent)
 
 tab.start()
 tab.call_method("Network.enable")
-tab.call_method("Page.navigate", url=TARGET_URL, _timeout=5)
+tab.call_method("Page.navigate", url=target_url, _timeout=5)
 
 tab.wait(5)
 
